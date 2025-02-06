7cb15af38faf62874e4243fbee5566d52046e4ee
richardmai <richardmai@debian-huy.168.1.10.non-exists.ptr.local>
2025-02-05 23:07:09 -0800

    add: model

---
+++ amplify/auth/post-confirmation/handler.ts
@@ -0,0 +1,49 @@
+import { Amplify } from "aws-amplify";
+import type { PostConfirmationTriggerHandler } from "aws-lambda";
+import { env } from "$amplify/env/post-confirmation";
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "../../data/resource";
+import { createUserProfile } from "./graphql/mutations";
+
+Amplify.configure({
+	API: {
+		GraphQL: {
+			endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+			region: env.AWS_REGION,
+			defaultAuthMode: 'iam'
+		},
+	},
+
+}, {
+	Auth: {
+		credentialsProvider: {
+			getCredentialsAndIdentityId: async () => ({
+				credentials: {
+					accessKeyId: env.AWS_ACCESS_KEY_ID,
+					secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+					sessionToken: env.AWS_SESSION_TOKEN
+				},
+			}),
+			clearCredentialsAndIdentityId: () => {
+				/** noop */
+			},
+		},
+	},
+});
+
+const client = generateClient<Schema>({
+	authMode: 'iam',
+});
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+	await client.graphql({
+		query: createUserProfile,
+		variables: {
+			input: {
+				email: event.request.userAttributes.email,
+				profileOwner: `${event.request.userAttributes.sub}::${event.userName}`,
+			}
+		}
+	})
+	return event;
+};
